d3a90f196e552d501a57a16f48c074b930652fdd
Luka Goginashvili <145713150+blodwenlixiss@users.noreply.github.com>
2024-10-05 17:08:10 +0400

    added lazy to single page

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,10 @@ import DefaultLayout from "./components/layout/default/defaultLayout";
 import About from "./pages/about/view";
 import Countries from "./pages/contact/view";
 import { lazy, Suspense } from "react";
-import SinglePage from "./pages/home/view/singlePage";
+
 const HomePage = lazy(() => import("./pages/home/view/initalPage/index"));
+const SinglePage = lazy(() => import("./pages/home/view/singlePage"));
+
 function App() {
   return (
     <BrowserRouter>

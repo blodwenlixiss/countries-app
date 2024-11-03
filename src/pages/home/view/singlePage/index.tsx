84f6813fecaf5c7b975203c22c9aef82c84b80cc
Luka Goginashvili <145713150+blodwenlixiss@users.noreply.github.com>
2024-11-04 00:22:51 +0400

    database Seed added

--- src/pages/home/view/singlePage/index.tsx
+++ src/pages/home/view/singlePage/index.tsx
@@ -1,21 +1,21 @@
-import { useParams } from "react-router-dom";
-import MainSinglePage from "../../components/singlePage/singlePage";
-import axios from "axios";
-import { useEffect } from "react";
+// import { useParams } from "react-router-dom";
+// import MainSinglePage from "../../components/singlePage/singlePage";
+// import axios from "axios";
+// import { useEffect } from "react";
 
-const SinglePage = () => {
-  const params = useParams();
-  const lang = params.lang as string;
-  if (!findPage) {
-    return <div style={{ marginTop: "200px" }}>Page was not found</div>;
-  }
+// const SinglePage = () => {
+//   const params = useParams();
+//   const lang = params.lang as string;
+//   if (!findPage) {
+//     return <div style={{ marginTop: "200px" }}>Page was not found</div>;
+//   }
 
-  return (
-    <MainSinglePage
-      flag={findPage.flag}
-      title={findPage.title}
-      population={findPage.population}
-    />
-  );
-};
-export default SinglePage;
+//   return (
+//     <MainSinglePage
+//       flag={findPage.flag}
+//       title={findPage.title}
+//       population={findPage.population}
+//     />
+//   );
+// };
+// export default SinglePage;
